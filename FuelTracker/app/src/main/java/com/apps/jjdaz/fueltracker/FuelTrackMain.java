cacc2627b23293e69303b594cf202b16dade741e
Justin Daza <justinjo@ualberta.ca>
2016-01-29 13:59:11 -0700

    Implemented some buttons

--- FuelTracker/app/src/main/java/com/apps/jjdaz/fueltracker/FuelTrackMain.java
+++ FuelTracker/app/src/main/java/com/apps/jjdaz/fueltracker/FuelTrackMain.java
@@ -9,11 +9,39 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 
 public class FuelTrackMain extends AppCompatActivity {
+    //implement data storage in a file labelled file.sav
+    private static final String FILENAME = "file.sav";
+    private EditText bodyText;
+    private ListView logEntryList;
 
+    private ArrayList<LogEntry> logs = new ArrayList<LogEntry>();
+    private ArrayAdapter<LogEntry> adapter;
+    private Float totalFuelCost;
+
+    // Activity should display a list of all the entries, and a calculated value of the total cost from the entries.
+    // Contains 2 buttons, one that goes opens another activity where user enters information for a new entry.
+    // Second button clears all current entries. (Optional)
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +49,31 @@ public class FuelTrackMain extends AppCompatActivity {
         setContentView(R.layout.activity_fuel_track_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        logEntryList = (ListView) findViewById(R.id.logentrylist);
+        Button newEntryButton = (Button) findViewById(R.id.addnewbutton);
+        Button ClearEntryButton = (Button) findViewById(R.id.clearall);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        //Send Array list to be edited in NewEntry activity
+        //Source code from Ravind Maurya; http://stackoverflow.com/questions/21250339/how-to-pass-arraylistcustomeobject-from-one-activity-to-another 2016-01-29
+
+
+        //implement buttons on main activity
+        //newEntry button goes to new entry activity
+        newEntryButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                startActivity(new Intent(FuelTrackMain.this, NewEntry.class));
+            }
+        });
+        ClearEntryButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                //clear tweets on disk & remove tweets on screen
+                logs.clear();
+                adapter.notifyDataSetChanged();
+                saveInFile();
+
             }
         });
     }
@@ -53,6 +99,52 @@ public class FuelTrackMain extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
 
+    }
+    @Override
+    protected void onStart() {
+        // TODO Auto-generated method stub
+        super.onStart();
+
+        loadFromFile();
+        // CHeck what this line is for corectness
+        adapter = new ArrayAdapter<LogEntry>(this, R.layout.activity_list_entry_edit, logs);
+        logEntryList.setAdapter(adapter);
+    }
+    //Code from lonelyTwitter application from Lab sessions
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+            Gson gson = new Gson();
+
+            //Source: https://google-gson.googlecode.com/svn/trunk/gson/docs/javadocs/com/google/gson/Gson.html 01-17-2016
+            Type listType = new TypeToken<ArrayList<LogEntry>>() {}.getType();
+            logs = gson.fromJson(in, listType);
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            logs = new ArrayList<LogEntry>();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, 0);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+            Gson gson = new Gson();
+            gson.toJson(logs, out);
+            out.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            throw new RuntimeException();
+        }
     }
 
 }
--- FuelTracker/app/src/main/java/com/apps/jjdaz/fueltracker/NewEntry.java
+++ FuelTracker/app/src/main/java/com/apps/jjdaz/fueltracker/NewEntry.java
@@ -1,10 +1,17 @@
 package com.apps.jjdaz.fueltracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class NewEntry extends AppCompatActivity {
+import java.util.ArrayList;
 
+
+public class NewEntry extends AppCompatActivity {
+    //Activity should take in various inputs and store them in the appropriate areas.
+    //Raise exceptions when invalid inputs are entered.
+    //"Cancel Entry" button returns user to main activity window, with no inputs saved
+    //"Save Entry" button validates and takes in all inputs and stores it into a new logEntry object, which is saved into
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
